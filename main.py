58212256fb0dbcf8834bd4fbc0df3fd48e4ed0da
Ankur mishra <140981811+iam-ankur01@users.noreply.github.com>
2026-05-27 14:57:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFacePipeline
 
@@ -28,13 +29,87 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
 
-    # Save PDF temporarily
     pdf_path = "temp.pdf"
 
     with open(pdf_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
     st.success("PDF Uploaded Successfully!")
+
+    loader = PyPDFLoader(pdf_path)
+
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100
+    )
+
+    docs = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    vectorstore = FAISS.from_documents(
+        docs,
+        embeddings
+    )
+
+    retriever = vectorstore.as_retriever()
+
+    pipe = pipeline(
+        "text2text-generation",
+        model="google/flan-t5-small",
+        max_length=256
+    )
+
+    llm = HuggingFacePipeline(
+        pipeline=pipe
+    )
+
+    prompt = PromptTemplate(
+        template="""
+        Answer the question using the context below.
+
+        Context:
+        {context}
+
+        Question:
+        {input}
+
+        Answer:
+        """,
+        input_variables=["context", "input"]
+    )
+
+    document_chain = create_stuff_documents_chain(
+        llm,
+        prompt
+    )
+
+    retrieval_chain = create_retrieval_chain(
+        retriever,
+        document_chain
+    )
+
+    query = st.text_input(
+        "Ask your question"
+    )
+
+    if query:
+
+        with st.spinner("Generating Answer..."):
+
+            response = retrieval_chain.invoke({
+                "input": query
+            })
+
+        st.subheader("Answer")
+
+        st.write(response["answer"])
+
+    os.remove(pdf_path)    st.success("PDF Uploaded Successfully!")
 
     # Load PDF
     loader = PyPDFLoader(pdf_path)
